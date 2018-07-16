4d10cd9ed9a6d32195de2f1487ecdd6e4218ba1d
CL <CL@DESKTOP-57V6TIO>
2018-07-16 13:51:07 +0300

    Дабавлена выборка на все книги

--- mysql/1.sql
+++ mysql/1.sql
@@ -1,8 +1,13 @@
-
+// У которых есть автор
 SELECT b.id, b.name, COUNT(p.author_id) AS count_authors
 FROM books AS b
 JOIN author_book AS p 
 ON (b.id = p.book_id)
 GROUP BY b.id
 
-
+// Или все книги
+SELECT b.id, b.name, COUNT(p.author_id) AS count_authors
+FROM books AS b
+LEFT JOIN author_book AS p 
+ON (b.id = p.book_id)
+GROUP BY b.id
